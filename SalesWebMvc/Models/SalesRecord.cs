4acec80640977b441d202200191c0234e00520a9
Fernando Xavier <fer-xavier@hotmail.com>
2019-06-22 21:22:07 -0300

    Chave estrangeira não nula na tabela Seller

---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,29 @@
+﻿using SalesWebMvc.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord() { }
+
+        public SalesRecord(int id, DateTime daate, double amount, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = Date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+
+    }
+}
